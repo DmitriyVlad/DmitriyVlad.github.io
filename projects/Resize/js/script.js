7edbc9305944d30b4a11cae85a17964842fa3751
DmitriyVlad <dmitriy.vladislavovich.93@gmail.com>
2016-07-03 15:34:25 +0300

    Resize fixed

--- projects/Resize/js/script.js
+++ projects/Resize/js/script.js
@@ -1,30 +1,57 @@
 $(function() {
-  var prevTextWidth, currentTextWidth;
-  var text = $('.text');
-  var textWidth = text.width();
-  var displayWidth = $(window).width();
+  // var prevTextWidth, currentTextWidth;
+  // var text = $('.text');
+  // var textWidth = text.width();
+  // var displayWidth = $(window).width();
 
-  console.log(textWidth);
-  console.log(displayWidth)
+  // console.log('textWidth: ' + textWidth);
+  // console.log('displayWidth: ' + displayWidth)
 
-  if (displayWidth >= 320 && displayWidth <= 380) {
-    text.css({'color': 'red'});
+  // if (displayWidth >= 320 && displayWidth <= 380) {
+  //   text.css({'color': 'red'});
+  // }
+
+  // $(window).resize(function() {
+  //   prevTextWidth = currentTextWidth;
+  //   currentTextWidth = text.width();
+
+  //   if (currentTextWidth < textWidth && prevTextWidth === currentTextWidth) {
+  //     text.css({'color' : 'red'});
+  //   } else {
+  //     text.css({'color' : 'grey'});
+  //   }
+
+  //   console.log('prevTextWidth: ' + prevTextWidth);
+  //   console.log('currentTextWidth: ' + currentTextWidth);
+  // });
+
+  var left = $('.left'),
+      right = $('.right'),
+      leftWidth = left.width(),
+      rightWidth = right.width() - 1,
+      prevLeftWidth,
+      prevRightWidth;
+
+  if (leftWidth < rightWidth) {
+    $('.text').css({'color': 'red'});
   }
 
   $(window).resize(function() {
-    prevTextWidth = currentTextWidth;
-    currentTextWidth = text.width();
+    leftWidth = left.width();
+    rightWidth = right.width() - 1;
 
-    if (currentTextWidth < textWidth && prevTextWidth === currentTextWidth) {
-      text.css({'color' : 'red'});
+    if (leftWidth < rightWidth) {
+      $('.text').css({'color': 'red'});
     } else {
-      text.css({'color' : 'grey'});
+      $('.text').css({'color': 'grey'});
     }
 
-    console.log(prevTextWidth);
-    console.log(currentTextWidth);
+    console.log('leftWidth: ' + leftWidth);
+    console.log('rightWidth: ' + rightWidth);
   });
 
+  console.log(leftWidth);
+  console.log(rightWidth);
 });
 
 
